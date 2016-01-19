5765e6dc414f504cf8d755999bca800e111ac150
Brock Whitten <brock@chloi.io>
2016-01-18 17:17:46 -0800

    includes namespace

--- test/basic.js
+++ test/basic.js
@@ -11,21 +11,34 @@ describe("basic", function(){
     done()
   })
 
-  it("should alert in 2 seconds", function(done){
+  it("should alert in 1 seconds", function(done){
     sub.on("sendReminderEmail", function(email, name){
       email.should.eql("brock@sintaxi.com")
       name.should.eql("brock")
       done()
     })
-    pub.schedule(2, "sendReminderEmail", ["brock@sintaxi.com", "brock"])
+    pub.schedule(1, "sendReminderEmail", ["brock@sintaxi.com", "brock"])
   })
 
-  it("should alert in 7 is seconds", function(done){
+  it("should alert in 3 seconds", function(done){
     sub.on("removeProject", function(domain){
       domain.should.eql("sintaxi.com")
       done()
     })
-    pub.schedule(7, "removeProject", ["sintaxi.com"])
+    pub.schedule(3, "removeProject", ["sintaxi.com"])
+  })
+
+  it("should alert in 8 seconds", function(done){
+    sub.on("removeProject", function(domain){
+      domain.should.eql("bart.com")
+      done()
+    })
+
+    // here we continually reset the fuse by including a namespace.
+    pub.schedule(3, "removeProject", "namespace", ["foo.com"])
+    pub.schedule(4, "removeProject", "namespace", ["bar.com"])
+    pub.schedule(5, "removeProject", "namespace", ["baz.com"])
+    pub.schedule(6, "removeProject", "namespace", ["bart.com"])
   })
 
 })
